1e9f3d122cf8294444d86d838148318ac7e8de58
wangqi <1942460489@qq.com>
2023-12-10 20:19:17 +0800

    fix: base error

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
+  base: '/scaleph-repress-site/',
   title: 'Scaleph',
   description: '开放数据平台，具备 Flink 和 SeaTunnel 任务管理能力',
   icon: '/scaleph-icon.png',
