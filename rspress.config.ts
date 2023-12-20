918420d785610b65636f9a66bac37da42546402a
wangqi <1942460489@qq.com>
2023-12-20 09:18:57 +0800

    feature: update 2.0.1 version docs

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,49 +1,49 @@
 import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import {defineConfig} from 'rspress/config';
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  base: '/scaleph-repress-site/',
-  title: 'Scaleph',
-  description: '开放数据平台，具备 Flink 和 SeaTunnel 任务管理能力',
-  icon: '/scaleph-icon.png',
-  logo: '/scaleph-icon.png',
-  lang: 'zh',
-  multiVersion: {
-    default: '2.0.0',
-    versions: ['dev', '2.0.0', '1.0.4'],
-  },
-  themeConfig: {
-    lastUpdated: true,
-    socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/flowerfine/scaleph' },
-      { icon: 'wechat', mode: 'img', content: '/wechat.jpg' },
-    ],
-    locales: [
-      {
-        lang: 'zh',
-        label: '简体中文',
-        editLink: {
-          docRepoBaseUrl:
-              'https://github.com/flowerfine/scaleph-repress-site/tree/main/docs',
-          text: '📝 在 GitHub 上编辑此页',
-        },
-        prevPageText: '上一篇',
-        nextPageText: '下一篇',
-        outlineTitle: '目录',
-      },
-      {
-        lang: 'en',
-        label: 'English',
-        editLink: {
-          docRepoBaseUrl:
-              'https://github.com/flowerfine/scaleph-repress-site/tree/main/docs',
-          text: '📝 Edit this page on GitHub',
-        },
-      },
-    ],
-  },
-  markdown: {
-    checkDeadLinks: false
-  }
+    root: path.join(__dirname, 'docs'),
+    base: '/scaleph-repress-site/',
+    title: 'Scaleph',
+    description: '开放数据平台，具备 Flink 和 SeaTunnel 任务管理能力',
+    icon: '/scaleph-icon.png',
+    logo: '/scaleph-icon.png',
+    lang: 'zh',
+    multiVersion: {
+        default: '2.0.1',
+        versions: ['2.0.1', '2.0.0', '1.0.4'],
+    },
+    themeConfig: {
+        lastUpdated: true,
+        socialLinks: [
+            {icon: 'github', mode: 'link', content: 'https://github.com/flowerfine/scaleph'},
+            {icon: 'wechat', mode: 'img', content: '/wechat.jpg'},
+        ],
+        locales: [
+            {
+                lang: 'zh',
+                label: '简体中文',
+                editLink: {
+                    docRepoBaseUrl:
+                        'https://github.com/flowerfine/scaleph-repress-site/tree/main/docs',
+                    text: '📝 在 GitHub 上编辑此页',
+                },
+                prevPageText: '上一篇',
+                nextPageText: '下一篇',
+                outlineTitle: '目录',
+            },
+            {
+                lang: 'en',
+                label: 'English',
+                editLink: {
+                    docRepoBaseUrl:
+                        'https://github.com/flowerfine/scaleph-repress-site/tree/main/docs',
+                    text: '📝 Edit this page on GitHub',
+                },
+            },
+        ],
+    },
+    markdown: {
+        checkDeadLinks: false
+    }
 });
